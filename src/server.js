3372af2a1bad38fb4f41fab39beb174b001d12f4
front <frontbae@ictus.app>
2023-06-20 01:33:09 +0900

    - 소켓을 활용한 간단한 채팅 구현 해보기

--- src/server.js
+++ src/server.js
@@ -16,9 +16,31 @@ const PORT = 3000;
 
 const server = http.createServer(app);
 const wss = new WebSocketServer({server}); // 포트를 공유하여, http 서버와 ws 서버를 생성
+
+const sockets = [];
 wss.on("connection", (socket) => {
-    console.log("connection");
-    console.log(socket);
+    // console.log("connection");
+    // console.log(socket);
+    sockets.push(socket);
+    socket['nickName'] = 'Unknown';
+    socket.on("close", () => console.log("소켓 연결 끊김.."));
+    socket.on("message", (message) => {
+        console.log(message);
+        // socket.send(message.toString('utf8'));
+        const targetMessage = JSON.parse(message);
+        sockets.forEach(targetSocket => {
+            switch (targetMessage.type) {
+                case 'nickName' :
+                    socket['nickName'] = targetMessage.payload.toString('utf8');
+                    break;
+                case 'message':
+                    targetSocket.send(`${socket['nickName']}: ${targetMessage.payload.toString('utf8')}`);
+                    break;
+
+            }
+        });
+    });
+    socket.send("Welcome Socket");
 });
 
 server.listen(PORT, () => {
