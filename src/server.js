dec76e65ce8e4597c24137837c98c030a249a5ba
front <frontbae@ictus.app>
2023-06-19 10:06:44 +0900

    - server.js 수정 express 서버위에 ws 서버 연동(http 서버에 ws 서버까지 같이)

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import * as http from 'http';
+import WebSocket, { WebSocketServer } from 'ws';
 import express from 'express';
 const app = express();
 
@@ -7,4 +9,14 @@ app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000)
+const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`listen ${PORT}`);
+// });
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({server}); // 포트를 공유하여, http 서버와 ws 서버를 생성
+
+server.listen(PORT, () => {
+    console.log(`listen ${PORT}`);
+});
